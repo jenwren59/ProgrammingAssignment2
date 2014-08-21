5d42bdee6bf56c2e9b20dd7e0fb719680b6e0d8f
jenwren59 <jenwren59@gmail.com>
2014-08-20 21:22:52 -0400

    Submitting programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
+
+###################################################################
 ## Put comments here that give an overall description of what your
 ## functions do
+## In this example we introduce the `<<-` operator which can be used to
+## assign a value to an object in an environment that is different from the
+## current environment (lexical scoping). 
+## Below are two functions that are used to create a
+## special object that stores a matrix and solves it's inverse
+##
+## I translated the example code for a vector - the lesson here was
+##  also how to use Git repositories
+###################################################################
+## JWhite August 2014
 
-## Write a short comment describing this function
-
+rm(list=ls())
+# Write a short comment describing this function
+## Create a matrix object that can cache it's inverse
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 
 }
 
 
 ## Write a short comment describing this function
-
+## computes the inverse of "special" matrix created in makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+### testing my function with one simple matrix
+mymat<-matrix(c(1.666,0.233,0.233,1.666), nrow=2, ncol=2)
+## Solve this the ordinary way
+#mymatI<-solve(mymat)
+#mymatI
+#mymat%*%mymatI
+
+## test the Cached solution
+y<-makeCacheMatrix(mymat)
+cacheSolve(y)
+mymat%*%cacheSolve(y)
